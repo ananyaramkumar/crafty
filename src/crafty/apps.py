d4725bca56081ad3b33ae14b019e669eac2b3012
Ananya Ramkumar <ananyaramkumar06@gmail.com>
2017-01-05 23:03:27 -0600

    making the app heroku friendly

--- src/crafty/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CraftyConfig(AppConfig):
-    name = 'crafty'
--- website/crafty/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Diy, Song
-
-admin.site.register(Diy)
-admin.site.register(Song)
